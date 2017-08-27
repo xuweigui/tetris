6157f43636298da32e0dda6f2052fd5e6479dead
Gary Xu <xuweigui@hotmail.com>
2017-08-27 11:40:32 +1000

    first commit

---
+++ tetris.js
@@ -0,0 +1,40 @@
+const canvas = document.getElementById("tetris");
+//window.alert(canvas);
+const context = canvas.getContext('2d');
+
+context.scale(20, 20);
+
+const matrix =[
+    [0,0,0],
+    [1,1,1] ,
+    [0,1,0],
+];
+
+const player = {
+  pos: {x:5, y:5},
+  matrix: matrix
+}
+
+function drawMatrix(matrix, offset) {
+  matrix.forEach((row,y) => {
+    row.forEach((value, x) => {
+      if (value !== 0) {
+        context.fillStyle = 'orange';
+        context.fillRect(x + offset.x, y + offset.y, 1, 1);
+      }
+    });
+  });
+}
+
+function draw() {
+  context.fillStyle = '#A52D2C';
+  context.fillRect(0, 0, canvas.width, canvas.height);
+  drawMatrix(player.matrix, player.pos);
+}
+
+function update(time = 0) {
+  draw();
+  requestAnimationFrame(update);
+}
+
+update();
