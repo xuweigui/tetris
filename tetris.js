d99171d2ba9e330ee89b0eaee33449380d93a1a5
Gary Xu <xuweigui@hotmail.com>
2017-09-03 20:01:12 +1000

    change

--- tetris.js
+++ tetris.js
@@ -10,6 +10,15 @@ const matrix =[
     [0,1,0],
 ];
 
+function createMatrix(w,h) {
+ const matrix = [];
+  while (h--){
+      matrix.push(new Array(w).fill(0));
+  }
+return matrix
+
+}
+
 const player = {
   pos: {x:5, y:5},
   matrix: matrix
@@ -32,9 +41,43 @@ function draw() {
   drawMatrix(player.matrix, player.pos);
 }
 
+function playerDrop() {
+  player.pos.y++;
+  dropCounter = 0;
+}
+
+let dropCounter = 0;
+let dropInterval = 1000;
+
+let lastTime = 0;
+
+
+
 function update(time = 0) {
+  console.log(lastTime);
+  const deltaTime = time - lastTime;
+  //console.log("dalta = " + deltaTime);
+  lastTime = time;
+
+  dropCounter += deltaTime;
+
+  //console.log(dropCounter);
+  if (dropCounter > dropInterval) {
+    
+    playerDrop();
+  }
   draw();
   requestAnimationFrame(update);
 }
+
+document.addEventListener('keydown',event => {
+ if (event.keyCode === 37) {
+  player.pos.x--;
+ } else if (event.keyCode ===39) {
+  player.pos.x++;
+ } else if (event.keyCode === 40) {
+  playerDrop();
+ } 
+});
 
 update();
